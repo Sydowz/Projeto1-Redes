0df023bbce58069b2293aa7d2b8ef8fdd0635fcc
Sydow <jonitossydow1@gmail.com>
2026-03-09 17:47:24 -0300

    Add threading for message handling in client

--- cliente.py
+++ cliente.py
@@ -12,11 +12,55 @@ def conectar_cliente():
     return client
 
 
+def thread_receber_mensagem(client):
+    """Fica em loop recebendo mensagens do servidor."""
+    while True:
+        try:
+            message = client.recv(1024).decode()
+        except OSError:
+            break
+
+        if not message:
+            print("Leilao encerrado! Pressione Enter para sair.")
+            break
+
+        print(message, end="")
+
+
+def thread_enviar_mensagem(client):
+    """Fica em loop lendo entrada do usuario e enviando ao servidor."""
+    while True:
+        try:
+            message = input()
+        except EOFError:
+            message = ":quit"
+
+        try:
+            client.send(message.encode())
+        except OSError:
+            break
+
+        if message == ":quit":
+            break
+
+
 def main_cliente():
     """Ponto de entrada do cliente."""
     client = conectar_cliente()
-    print("Cliente conectado ao servidor.")
-    client.close()
+
+    t_receive = threading.Thread(target=thread_receber_mensagem, args=(client,))
+    t_send = threading.Thread(target=thread_enviar_mensagem, args=(client,))
+
+    t_receive.start()
+    t_send.start()
+
+    t_receive.join()
+    t_send.join()
+
+    try:
+        client.close()
+    except OSError:
+        pass
 
 
 if __name__ == "__main__":
